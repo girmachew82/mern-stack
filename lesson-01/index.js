f1aa9739d49295b0fd526758808dc897631662f7
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-04-16 20:56:22 +0300

    lesson 01 index.js

--- lesson-01/index.js
+++ lesson-01/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
-const port  = 3003
+const router = express.Router()
+const port  = 10000
+
+app.get('/',(req, res)=>{
+    res.send("Hello World")
+})
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 })

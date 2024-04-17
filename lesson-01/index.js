a1a1ecdabb23f294d374b552c68cd40047ccf6d9
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-04-17 21:29:26 +0300

    Routes

--- lesson-01/index.js
+++ lesson-01/index.js
@@ -2,7 +2,23 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const port  = 10000
-const cors = require('cors')
+const path = require('path')
+
+app.use('/', express.static(path.join(__dirname,'public')))
+
+app.use('/', require('./routes/root'))
+app.all('*',(req, res)=>{
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname,'views','404.html'))
+    }else if(req.accepts('json')){
+        res.json({
+            message:"404 Not found"
+        })
+        }else{
+            res.type('txt').send('404 Not Found')
+        }
+})
 app.get('/',(req, res)=>{
     res.send("Hello World")
 })

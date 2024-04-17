84731cebce6975c812cc067a12eef25ebfe5731c
Girmachew Gulint Tilahun <58398921+girmachew82@users.noreply.github.com>
2024-04-17 11:24:53 +0300

    some dependecies

--- lesson-01/index.js
+++ lesson-01/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const router = express.Router()
 const port  = 10000
-
+const cors = require('cors')
 app.get('/',(req, res)=>{
     res.send("Hello World")
 })
--- lesson-02/index.js
+++ lesson-02/index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
+const {logger} = require('./middleware/logger')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+
 const port = process.env.PORT
+
+//custom middleware
+app.use(logger)
+
+app.use(express.json())
+app.use(cookieParser())
+app.get('/',(req, res)=>{
+    res.send("Hello World")
+})
+//custom middleware
+app.use(errorHandler)
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`)
 })
